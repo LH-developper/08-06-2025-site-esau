29b2a32a7bdd509e50ab04fa34fc1fb110cdd0f8
Eliot de Pins <eliotdepins@MacBook-Air-de-Eliot.local>
2025-06-08 22:12:17 +0200

    Ajout du widget Chatbase dans ClientLayout

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,11 +1,41 @@
 'use client';
 
 import { useEffect } from "react";
+import Script from "next/script";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     console.log("Client component mounted");
   }, []);
 
-  return <>{children}</>;
+  return (
+    <>
+      {/* Script d'initialisation Chatbase */}
+      <Script
+        id="chatbase-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              if ((!window.chatbase || window.chatbase("getState") !== "initialized")) {
+                window.chatbase = (...args) => {
+                  if (!window.chatbase.q) window.chatbase.q = [];
+                  window.chatbase.q.push(args);
+                };
+              }
+            })();
+          `,
+        }}
+      />
+
+      {/* Chargement du widget Chatbase */}
+      <Script
+        src="https://www.chatbase.co/embed.min.js"
+        strategy="afterInteractive"
+      />
+
+      {/* Contenu du site */}
+      {children}
+    </>
+  );
 }
